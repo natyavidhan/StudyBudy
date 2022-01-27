d7d51658791941df3834e15a92606a59e520df0f
studiousgamer <natyavidhanbiswas10@gmail.com>
2022-01-27 10:49:20 +0530

    added authentication

--- app.py
+++ app.py
@@ -1,11 +1,36 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify, session
+from models import Database
+from authlib.integrations.flask_client import OAuth
+from loginpass import create_flask_blueprint, Google
+from dotenv import dotenv_values
+from datetime import datetime
 
 app = Flask(__name__)
-app.secret_key = "YOUR SECRET KEY"
+oauth = OAuth(app)
+config = dotenv_values(".env")
+config = dict(config)
+app.secret_key = config["secret_key"]
+for keys in config.keys():
+    app.config[keys] = config[keys]
+backends = [Google]
+database = Database()
 
 @app.route('/')
 def index():
+    if 'user' in session:
+        return render_template('user.html', user=session['user'])
     return render_template('index.html')
+
+def handle_authorize(remote, token, user_info):
+    if database.userExists(user_info['email']):
+        session['user'] = database.getUserByEmail(user_info['email'])
+    else:
+        database.addUser(user_info['email'])
+        session['user'] = database.getUserByEmail(user_info['email'])
+    return redirect(url_for('index'))
+
+bp = create_flask_blueprint(backends, oauth, handle_authorize)
+app.register_blueprint(bp, url_prefix='/')
     
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,29 @@
+from pymongo import MongoClient
+from uuid import uuid4
+import requests
+from datetime import datetime
+import json
+from dotenv import dotenv_values
+
+class Database:
+    def __init__(self):
+        self.config = dotenv_values(".env")
+        self.db = MongoClient(self.config["MONGOURL"]).StudyBudy
+        self.users = self.db.users
+        
+    def addUser(self, email):
+        user = {
+            "_id": str(uuid4()),
+            "email": email,
+            "name": email.split("@")[0]
+        }
+        self.users.insert_one(user)
+    
+    def getUserByEmail(self, email):
+        return self.users.find_one({"email": email})
+    
+    def getUser(self, id):
+        return self.users.find_one({"_id": id})
+    
+    def userExists(self, email):
+        return self.users.find_one({"email": email}) is not None
